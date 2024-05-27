b3f0de6ffab2a5291d475f5ccb541af024576e31
Pablo <pablonietoperez369@gmail.com>
2024-05-27 01:00:51 -0500

    Done

--- JS/script.js
+++ JS/script.js
@@ -17,18 +17,17 @@ function animate() {
   });
 
   $wrap.css({
-    'transform': 'translate(-50%, -50%) perspective(600px) rotateY(' + -x + 'deg) rotateX(' + y + 'deg)'
+    'transform': 'translate(-50%, -50%) perspective(600px) rotateY(' + -x 
+    + 'deg) rotateX(' + y + 'deg)'
   });
   window.requestAnimationFrame(animate);
 }
 
 $(window).on('mousemove click', function(e) {
-
   var lMouseX = Math.max(-100, Math.min(100, $(window).width() / 2 - e.clientX));
   var lMouseY = Math.max(-100, Math.min(100, $(window).height() / 2 - e.clientY));
-  lFollowX = (12 * lMouseX) / 100; // 100 : 12 = lMouxeX : lFollow
+  lFollowX = (12 * lMouseX) / 100; // WIBINWEB
   lFollowY = (10 * lMouseY) / 100;
-
 });
 
 animate();
